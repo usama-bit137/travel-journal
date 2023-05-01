72eb323f690480a58f70480f03995f93adf82e4e
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-01 22:09:38 +0100

    background colour altered

--- src/Components/Destination.js
+++ src/Components/Destination.js
@@ -8,10 +8,13 @@ export default function Destination(props) {
             <img src={`./Images/${props.item.image.file}`} className="image" alt={props.item.image.alt}></img>
         </div>
         <article className="dest-info">
-            <span className="dest-country"><img src={ping} width="20px"></img><p>{props.item.country}</p></span>
+            <span className="dest-country">
+                <img src={ping} width="20px"></img>
+                <p>{props.item.country}</p>
+            </span>
             <h2 className="dest-location">{props.item.location}</h2>
             <p className="dest-duration">{props.item.dates.startDate} - {props.item.dates.endDate}</p>
-            <p>{props.item.description}</p>
+            <p className="dest-description">{props.item.description}</p>
         </article>
         </div>
    )
