1be5ba47b725020d455a292b339c079fb2aad001
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-01 20:04:51 +0100

    Directory structure altered

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,23 @@
-import '../styles/App.css'
+import '../Styles/App.css'
 import React from "react"
+import Header from './Header'
+import Destination from './Destination';
+let destinations = require("../destinations.json"); // importing JSON file
 
 export default function App() {
+  let destinationsArray = destinations.map((dest) => {
+    return ([
+      <Destination
+        key = {dest.key}
+        item = {dest}
+      />
+    ])
+  })
+  console.log(destinationsArray);
   return (
-    <h1>Travel Journal</h1>
+    <>
+      <Header />
+      {destinationsArray}
+    </>
   )
 }
